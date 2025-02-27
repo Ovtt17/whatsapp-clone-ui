9556c43c052931a38deaf70ea5ce637f460bfef0
Ovett Mora <ovettmora17@gmail.com>
2025-02-27 17:48:48 -0600

    Add ChatContent component as a placeholder for chat display

---
+++ src/modules/chat/components/ChatContent/ChatContent.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+interface ChatContentProps {
+  // define props here
+}
+
+const ChatContent: FC<ChatContentProps> = ({  }) => {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default ChatContent;
