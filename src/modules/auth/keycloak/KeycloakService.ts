6f229b507b3286604065003dfa461fbff1348608
Ovett Mora <ovettmora17@gmail.com>
2025-02-27 17:12:14 -0600

    Add KeycloakService for authentication management

---
+++ src/modules/auth/keycloak/KeycloakService.ts
@@ -0,0 +1,47 @@
+import Keycloak from 'keycloak-js';
+
+class KeycloakService {
+  private keycloak: Keycloak;
+
+  constructor() {
+    this.keycloak = new Keycloak({
+      realm: import.meta.env.VITE_KEYCLOAK_REALM,
+      url: import.meta.env.VITE_KEYCLOAK_URL,
+      clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID
+    });
+  }
+
+  async init() {
+    return await this.keycloak.init({onLoad: 'login-required'});
+  }
+
+  async login() {
+    await this.keycloak.login();
+  }
+
+  async logout() {
+    await this.keycloak.logout({ redirectUri: import.meta.env.VITE_APP_URL });
+  }
+
+  isTokenValid() {
+    return !this.keycloak.isTokenExpired();
+  }
+
+  async accountManagement() {
+    await this.keycloak.accountManagement();
+  }
+
+  get userId(): string | null {
+    return this.keycloak.tokenParsed?.sub || null;
+  }
+
+  get fullName(): string | null {
+    return this.keycloak.tokenParsed?.['name'] || null;
+  }
+
+  get token(): string | null {
+    return this.keycloak.token || null;
+  }
+}
+
+export default new KeycloakService();
