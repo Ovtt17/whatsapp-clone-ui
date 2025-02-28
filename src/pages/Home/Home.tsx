c3c08f5e7d1dbf6e402b5a5bd57515fd56153192
Ovett Mora <ovettmora17@gmail.com>
2025-02-27 23:59:52 -0600

    Replace ChatsAside with ChatList in Home component for improved chat display

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,11 @@
-import {FC} from 'react';
-import ChatsAside from "../../modules/chat/components/Aside/ChatsAside.tsx";
+import ChatList from '@/modules/chat/components/ChatList';
+import { FC } from 'react';
 
 const Home: FC = () => {
   return (
-    <section className="flex justify-center items-center min-h-screen">
-      <header className="absolute top-0 w-full h-1/5 bg-[#1ea884]"></header>
-      <main className="flex relative w-4/5 max-w-full h-[calc(100vh-40px)] bg-white shadow-2xl">
-        <ChatsAside />
-      </main>
-    </section>
+    <>
+      <ChatList />
+    </>
   );
 }
 
