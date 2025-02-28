848fad10cb3b746cf780320431787502889df3a0
Ovett Mora <ovettmora17@gmail.com>
2025-02-28 01:07:02 -0600

    Wrap AppRoutes with ChatProvider for chat context management

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes.tsx";
 import { KeycloakProvider } from "./modules/auth/keycloak/KeycloakContext.tsx";
 import { DefaultLayout } from './layout/DefaultLayout';
+import { ChatProvider } from "./modules/chat/context/ChatContext.tsx";
 
 function App() {
   return (
     <BrowserRouter>
       <KeycloakProvider>
         <DefaultLayout>
-          <AppRoutes />
+          <ChatProvider>
+            <AppRoutes />
+          </ChatProvider>
         </DefaultLayout>
       </KeycloakProvider>
     </BrowserRouter>
