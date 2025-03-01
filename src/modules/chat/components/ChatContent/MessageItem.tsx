b3dc27357024ddb544d2a83a35c8bb9e78755cf5
Ovett Mora <ovettmora17@gmail.com>
2025-03-01 01:32:18 -0600

    Refactor MessageItem component for improved layout and readability

--- src/modules/chat/components/ChatContent/MessageItem.tsx
+++ src/modules/chat/components/ChatContent/MessageItem.tsx
@@ -11,33 +11,29 @@ interface MessageItemProps {
 
 const MessageItem: FC<MessageItemProps> = ({ message, isSelf }) => (
   <div className={`message-box ${isSelf ? 'self' : 'friend'}`}>
-    <div>
-      <div className='flex flex-col items-end'>
-        {message.type === MessageType.TEXT && (
-          <span>{message.content}</span>
-        )}
-        {message.media && (
-          <img
-            width='200'
-            className='cursor-pointer'
-            src={`data:image/jpg;base64,${message.media}`}
-            alt='chat media'
+    <div className={`flex flex-col ${!isSelf && 'items-end'}`}>
+      {message.type === MessageType.TEXT && (
+        <span>{message.content}</span>
+      )}
+      {message.media && (
+        <img
+          width='200'
+          className='cursor-pointer rounded-lg'
+          src={`data:image/jpg;base64,${message.media}`}
+          alt='chat media'
+        />
+      )}
+      <div className='flex gap-2 justify-end items-center text-sm'>
+        <small className='text-gray-600'>{dayjs(message.createdAt).format('HH:mm')}</small>
+        {isSelf && (
+          <IoCheckmarkDoneSharp
+            className={`h-4 w-4 ${message.state === MessageState.SENT ? 'text-gray-500' : 'text-blue-600'}`}
           />
         )}
-        <div className='flex gap-2 justify-end items-center'>
-          <small className='text-black'>{dayjs(message.createdAt).format('HH:mm')}</small>
-          {message.state === MessageState.SENT && (
-            <span>
-              <IoCheckmarkDoneSharp className='h-4 w-4 text-gray-500' />
-            </span>
-          )}
-          {message.state === MessageState.SEEN && (
-            <IoCheckmarkDoneSharp className='h-4 w-4 text-blue-600 -ml-[7x]' />
-          )}
-        </div>
       </div>
     </div>
   </div>
 );
+
 
 export default MessageItem;
