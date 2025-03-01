cb39be24929cf8f3624fd0e6542a62de50401dd4
Ovett Mora <ovettmora17@gmail.com>
2025-03-01 01:32:14 -0600

    Refactor ChatListHeader to streamline icon click handling and improve readability

--- src/modules/chat/components/ChatList/ChatListHeader.tsx
+++ src/modules/chat/components/ChatList/ChatListHeader.tsx
@@ -3,16 +3,28 @@ import { useChatContext } from '../../context/ChatContext';
 import { FC } from 'react';
 
 const ChatListHeader: FC = () => {
-  const { searchNewContact, setSearchNewContact, searchContact} = useChatContext();
+  const { searchNewContact, setSearchNewContact, searchContact } = useChatContext();
+
+  const handleIconClick = () => {
+    if (searchNewContact) {
+      setSearchNewContact(false);
+    } else {
+      searchContact();
+    }
+  };
+
   return (
     <article className='flex justify-between items-center gap-2 p-2'>
       <h4 className='text-xl font-semibold'>Chat</h4>
-      {searchNewContact
-        ? <span onClick={searchContact}><FaCommentMedical className='h-6 w-6 text-black cursor-pointer' /></span>
-        : <span onClick={() => { setSearchNewContact(false) }} ><FaTimesCircle className='h-6 w-6 text-black cursor-pointer' /></span>
-      }
+      <span onClick={handleIconClick} className='cursor-pointer'>
+        {searchNewContact ? (
+          <FaTimesCircle className='h-6 w-6 text-black' />
+        ) : (
+          <FaCommentMedical className='h-6 w-6 text-black' />
+        )}
+      </span>
     </article>
   );
-}
+};
 
 export default ChatListHeader;
