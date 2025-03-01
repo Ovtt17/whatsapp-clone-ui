7ebd887c66a74fef702f69a68fc3b56334222379
Ovett Mora <ovettmora17@gmail.com>
2025-03-01 01:32:02 -0600

    Add setMessagesToSeen and saveMessage functions to handle message status updates and saving

--- src/modules/message/services/messageService.ts
+++ src/modules/message/services/messageService.ts
@@ -1,5 +1,6 @@
 import axiosInstance from "@/modules/auth/axios/axiosInstance";
 import { MessageResponse } from "../types/MessageResponse";
+import { MessageRequest } from "../types/MessageRequest";
 
 export const getMessages = async (chatId: string): Promise<MessageResponse[]> => {
   try {
@@ -7,6 +8,28 @@ export const getMessages = async (chatId: string): Promise<MessageResponse[]> =>
     return response.data;
   } catch (error) {
     console.error("Error getting messages:", error);
+    throw error;
+  }
+}
+
+export const setMessagesToSeen = async (chatId: string): Promise<void> => {
+  try {
+    await axiosInstance.patch(`/messages`, null, {
+      params: {
+        "chat-id": chatId,
+      }
+    });
+  } catch (error) {
+    console.error("Error setting messages to seen:", error);
+    throw error;
+  }
+}
+
+export const saveMessage = async (message: MessageRequest): Promise<Response> => {
+  try {
+    return await axiosInstance.post(`/messages`, message);
+  } catch (error) {
+    console.error("Error saving message:", error);
     throw error;
   }
 }
